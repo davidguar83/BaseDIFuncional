f9c376d44305ad9695cd1594b986fbe490aef05e
david83 <davidrguardiola@hotmail.es>
2021-06-05 00:19:05 +0200

    a

---
+++ ver_interfaz.py
@@ -0,0 +1,17 @@
+import gi
+from conexionBD import ConexionBD
+gi.require_version("Gtk","3.0")
+
+from gi.repository import Gtk
+
+class Ventana():
+
+    def __init__(self):
+        builder = Gtk.Builder()
+        builder.add_from_file("INTERFAZDI.glade")
+        VentanaDatos = builder.get_object("VentanaDatos")
+        VentanaDatos.show_all()
+
+if __name__ == "__main__":
+    Ventana()
+    Gtk.main()
