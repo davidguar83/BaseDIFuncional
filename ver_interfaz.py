c4b45deebbae6571d735fdad73c61b8adf9368f6
david83 <davidrguardiola@hotmail.es>
2021-06-05 01:50:35 +0200

    cambios codigo ver interfaz

--- ver_interfaz.py
+++ ver_interfaz.py
@@ -9,6 +9,15 @@ class Ventana():
     def __init__(self):
         builder = Gtk.Builder()
         builder.add_from_file("INTERFAZDI.glade")
+
+        sinais = {"win_delete_event": Gtk.main_quit()
+
+
+                  }
+
+
+
+
         VentanaDatos = builder.get_object("VentanaDatos")
         VentanaDatos.show_all()
 
